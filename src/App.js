931ffd7c7d9b8152c46c38e04ea72ed29f19b1db
Skolnieks <skolnieks.pjvt@pjvt.local>
2023-10-16 15:21:23 +0300

    yes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const [ratio, setRatio] = useState(0);
+  function PlusRatio() {
+    setRatio(ratio + 1);
+    console.log("RATIO +1");
+    console.log("ratio ir " + ratio);
+  }
+  const [nratio, setNratio] = useState(0);
+  function NegativeRatio() {
+    setNratio(nratio - 1);
+    console.log("NRATIO - 1");
+    console.log("nratio ir" + nratio);
+  }
+  const [gratio, setGratio] = useState(0);
+  function GuestRatio() {
+    
+  }
+  return ( 
+  <div className="App">
+    <button onClick={PlusRatio}>RATIO +1</button>
+    <button onClick={NegativeRatio}>RATIO -1</button>
+    <h1>{ratio}</h1>
+    <h2>{nratio}</h2>
+  </div>
   );
 }
 
